cb1f90ec7f3600d8bcf18f043133cb6d00a6750f
damaommmm <578159913@qq.com>
2019-10-15 20:43:46 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Button type="primary" size = "large">I am button</Button>
+        <Button type="primary" size = "large" disabled={true}>I am button</Button>
         <Button type="danger" size = "small">I am button</Button>
       </div>
     );
--- src/Button/Buttin.tsx
+++ src/Button/Buttin.tsx
@@ -5,16 +5,17 @@ import './Button.css'
 
 export interface IButtonProps {
     type:string,
-    size:string
+    size:string,
+    disabled?:boolean
 }
 
 
 class Button extends React.Component<IButtonProps> {
     public render() {
-        const {children,type,size} = this.props
-        const classname = classNames(type,size||"medium")
+        const {children,type,size,disabled} = this.props
+        const classname = classNames(type,size||"medium",disabled === true?"disabled":"")
         return (
-            <button className = {classname}>
+            <button className = {classname} disabled = {disabled}>
                 {children}
             </button>
         );
